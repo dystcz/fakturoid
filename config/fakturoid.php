925f10dc172766181c9182ec514bff713d8bb732
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-14 14:30:04 +0100

    update config

--- config/fakturoid.php
+++ config/fakturoid.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'account_api_id' => env('FAKTUROID_API_ID', 'XXX'),
-    'account_api_secret' => env('FAKTUROID_API_SECRET', 'XXX'),
+    'client_id' => env('FAKTUROID_CLIENT_ID', 'XXX'),
+    'client_secret' => env('FAKTUROID_CLIENT_SECRET', 'XXX'),
     'account_slug' => env('FAKTUROID_ACCOUNT_SLUG'),
     'user_agent' => env('FAKTUROID_USER_AGENT', 'Application <your@email.cz>'),
 ];
