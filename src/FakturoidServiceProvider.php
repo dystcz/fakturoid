42be50215115be57c467921cd0153a96c52c5b21
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-14 18:08:02 +0100

    bind fakturoid instance

--- src/FakturoidServiceProvider.php
+++ src/FakturoidServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Dystcz\Fakturoid;
 
 use Dystcz\Fakturoid\Contracts\Fakturoid as FakturoidContract;
+use Fakturoid\FakturoidManager;
+use GuzzleHttp\Client as Guzzle;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class FakturoidServiceProvider extends ServiceProvider
@@ -28,6 +32,18 @@ class FakturoidServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/fakturoid.php', 'fakturoid');
 
-        $this->app->singleton(FakturoidContract::class, fn () => new Fakturoid);
+        $this->app->bind('fakturoid', fn (Application $app) => $app->make(FakturoidManager::class, [
+            'client' => new Guzzle,
+            'clientId' => Config::get('fakturoid.client_id'),
+            'clientSecret' => Config::get('fakturoid.client_secret'),
+            'userAgent' => Config::get('fakturoid.user_agent'),
+            'accountSlug' => Config::get('fakturoid.account_slug'),
+        ]));
+
+        $this->app->singleton(
+            FakturoidContract::class,
+            fn (Application $app) => $app->make(Fakturoid::class, [
+                'fakturoid' => $app->make('fakturoid'),
+            ]));
     }
 }
